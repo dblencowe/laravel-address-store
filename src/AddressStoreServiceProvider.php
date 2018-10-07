ef01aa344293c237c6c62564dad5cbef1b0dcffa
Dave Blencowe <dblencowe@gmail.com>
2018-10-07 15:03:25 +0100

    Publish configuration file

--- src/AddressStoreServiceProvider.php
+++ src/AddressStoreServiceProvider.php
@@ -9,5 +9,8 @@ class AddressStoreServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/migrations');
+        $this->publishes([
+            __DIR__ . '/config/address_store.php' => config_path('address_store.php'),
+        ]);
     }
 }
